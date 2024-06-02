4b7c77f58af04335df2a30974b5a198e0b1cf48b
MarcoRudolph <mssayyy@gmail.com>
2024-06-02 20:44:57 +0200

    Initial commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
+  purge: ['./src/app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   plugins: {
     tailwindcss: {},
   },
